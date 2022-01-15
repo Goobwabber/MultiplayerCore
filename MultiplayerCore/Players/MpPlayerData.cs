1f06d7baef1b1f99aea569a3786b284f6faa98a8
goobwabber <littlehappytroll@gmail.com>
2022-01-15 15:15:34 -0500

    add playermanager event

---
+++ MultiplayerCore/Players/MpPlayerData.cs
@@ -0,0 +1,40 @@
+﻿using LiteNetLib.Utils;
+using MultiplayerCore.Networking.Abstractions;
+using System.Collections.Generic;
+
+namespace MultiplayerCore.Players
+{
+    public class MpPlayerData : MpPacket
+    {
+        /// <summary>
+        /// Platform User ID from <see cref="UserInfo.platformUserId"/>
+        /// </summary>
+        public string PlatformId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Platform from <see cref="UserInfo.platformUserId">
+        /// </summary>
+        public Platform Platform { get; set; }
+
+        public override void Serialize(NetDataWriter writer)
+        {
+            writer.Put(PlatformId);
+            writer.Put((int)Platform);
+        }
+
+        public override void Deserialize(NetDataReader reader)
+        {
+            PlatformId = reader.GetString();
+            Platform = (Platform)reader.GetInt();
+        }
+    }
+
+    public enum Platform
+    {
+        Unknown = 0,
+        Steam = 1,
+        OculusPC = 2,
+        OculusQuest = 3,
+        PS4 = 4
+    }
+}
--- MultiplayerCore/Players/MpPlayerManager.cs
+++ MultiplayerCore/Players/MpPlayerManager.cs
@@ -9,10 +9,12 @@ namespace MultiplayerCore.Players
 {
     public class MpPlayerManager : IInitializable, IDisposable
     {
-		public IReadOnlyDictionary<string, MpPlayer> Players => _playerData;
+		public event Action<IConnectedPlayer, MpPlayerData> PlayerConnectedEvent = null!;
+
+		public IReadOnlyDictionary<string, MpPlayerData> Players => _playerData;
 
 		private UserInfo _localPlayerInfo = null!;
-		private ConcurrentDictionary<string, MpPlayer> _playerData = new();
+		private ConcurrentDictionary<string, MpPlayerData> _playerData = new();
 
 		private readonly MpPacketSerializer _packetSerializer;
 		private readonly IPlatformUserModel _platformUserModel;
@@ -31,19 +33,19 @@ namespace MultiplayerCore.Players
 		public async void Initialize()
 		{
 			_sessionManager.SetLocalPlayerState("modded", true);
-			_packetSerializer.RegisterCallback<MpPlayer>(HandlePlayerData);
+			_packetSerializer.RegisterCallback<MpPlayerData>(HandlePlayerData);
 			_localPlayerInfo = await _platformUserModel.GetUserInfo();
             _sessionManager.playerConnectedEvent += HandlePlayerConnected;
 		}
 
         public void Dispose()
         {
-			_packetSerializer.UnregisterCallback<MpPlayer>();
+			_packetSerializer.UnregisterCallback<MpPlayerData>();
         }
 
 		private void HandlePlayerConnected(IConnectedPlayer player)
 		{
-			_sessionManager.Send(new MpPlayer
+			_sessionManager.Send(new MpPlayerData
 			{
 				PlatformId = _localPlayerInfo.platformUserId,
 				Platform = _localPlayerInfo.platform switch
@@ -57,12 +59,15 @@ namespace MultiplayerCore.Players
             });
 		}
 
-		private void HandlePlayerData(MpPlayer packet, IConnectedPlayer player)
+		private void HandlePlayerData(MpPlayerData packet, IConnectedPlayer player)
         {
 			_playerData[player.userId] = packet;
         }
 
-		public bool TryGetPlayer(string userId, out MpPlayer player)
+		public bool TryGetPlayer(string userId, out MpPlayerData player)
 			=> _playerData.TryGetValue(userId, out player);
+
+		public MpPlayerData? GetPlayer(string userId)
+			=> _playerData.ContainsKey(userId) ? _playerData[userId] : null;
 	}
 }
