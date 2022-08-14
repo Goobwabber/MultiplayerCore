e89d179d9809713a41e7d698314aa3916d1f0848
goobwabber <littlehappytroll@gmail.com>
2022-08-14 19:07:32 -0400

    Loading Indicator

--- MultiplayerCore/Installers/MpMenuInstaller.cs
+++ MultiplayerCore/Installers/MpMenuInstaller.cs
@@ -9,6 +9,7 @@ namespace MultiplayerCore.Installers
         {
             Container.BindInterfacesAndSelfTo<MpColorsUI>().AsSingle();
             Container.BindInterfacesAndSelfTo<MpRequirementsUI>().AsSingle();
+            Container.BindInterfacesAndSelfTo<MpLoadingIndicator>().AsSingle();
 
             // Inject sira stuff that didn't get injected on appinit
             Container.Inject(Container.Resolve<NetworkPlayerEntitlementChecker>());
---
+++ MultiplayerCore/UI/MpLoadingIndicator.cs
@@ -0,0 +1,69 @@
+﻿using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using Zenject;
+
+namespace MultiplayerCore.UI
+{
+    internal class MpLoadingIndicator : NotifiableBase, IInitializable, IDisposable, ITickable, IProgress<double>
+    {
+        public const string ResourcePath = "MultiplayerCore.UI.LoadingIndicator.bsml";
+
+        private readonly IMultiplayerSessionManager _sessionManager;
+        private readonly ILobbyGameStateController _gameStateController;
+        private readonly ILobbyPlayersDataModel _playersDataModel;
+        private readonly CenterStageScreenController _screenController;
+
+        private LoadingControl _loadingControl = null!;
+        private bool _isDownloading;
+
+        internal MpLoadingIndicator(
+            IMultiplayerSessionManager sessionManager,
+            ILobbyGameStateController gameStateController,
+            ILobbyPlayersDataModel playersDataModel,
+            CenterStageScreenController screenController)
+        {
+            _sessionManager = sessionManager;
+            _gameStateController = gameStateController;
+            _playersDataModel = playersDataModel;
+            _screenController = screenController;
+        }
+
+        public void Initialize()
+        {
+            BSMLParser.instance.Parse(BeatSaberMarkupLanguage.Utilities.GetResourceContent(Assembly.GetExecutingAssembly(), ResourcePath), _screenController.gameObject, this);
+            GameObject existingLoadingControl = Resources.FindObjectsOfTypeAll<LoadingControl>().First().gameObject;
+            GameObject loadingControlGO = GameObject.Instantiate(existingLoadingControl, _vert.transform);
+            _loadingControl = loadingControlGO.GetComponent<LoadingControl>();
+            _loadingControl.Hide();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void Tick()
+        {
+            if (_isDownloading)
+                return;
+            else if (_screenController.countdownShown && _sessionManager.syncTime >= _gameStateController.startTime && _gameStateController.levelStartInitiated)
+                _loadingControl.ShowLoading($"{_playersDataModel.Count(x => x.Value.isReady || !x.Value.isActive)} of {_playersDataModel.Count} players ready...");
+            else
+                _loadingControl.Hide();
+        }
+
+        public void Report(double value)
+        {
+            _isDownloading = value < 1.0;
+            _loadingControl.ShowDownloadingProgress($"Downloading ({value * 100:F2}%)...", (float)value);
+        }
+
+        [UIObject("vert")]
+        private GameObject _vert = null!;
+    }
+}
