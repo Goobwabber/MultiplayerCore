16eec4739f6c9d96b12d6545742669b604cbd428
rcelyte <rcelyte@battletrains.org>
2022-10-11 21:10:49 +0000

    fix NullReferenceException log spam in MpLoadingIndicator

--- MultiplayerCore/UI/MpLoadingIndicator.cs
+++ MultiplayerCore/UI/MpLoadingIndicator.cs
@@ -60,7 +60,7 @@ namespace MultiplayerCore.UI
         {
             if (_isDownloading)
                 return;
-            else if (_screenController.countdownShown && _sessionManager.syncTime >= _gameStateController.startTime && _gameStateController.levelStartInitiated)
+            else if (_screenController.countdownShown && _sessionManager.syncTime >= _gameStateController.startTime && _gameStateController.levelStartInitiated && _levelLoader.CurrentLoadingData != null)
                 _loadingControl.ShowLoading($"{_playersDataModel.Count(x => _entitlementChecker.GetUserEntitlementStatusWithoutRequest(x.Key, _levelLoader.CurrentLoadingData.beatmapLevel.beatmapLevel.levelID) == EntitlementsStatus.Ok)} of {_playersDataModel.Count} players ready...");
             else
                 _loadingControl.Hide();
