840d5362124b8853b05457d6a9b790b8b9505472
Roy de Jong <roy@softwarepunt.nl>
2023-07-05 21:14:53 +0200

    tweak: add "use_ssl" bool to MpStatusData

--- MultiplayerCore/Models/MpStatusData.cs
+++ MultiplayerCore/Models/MpStatusData.cs
@@ -33,9 +33,28 @@ namespace MultiplayerCore.Models
                 maximumAppVersion = value;
             }
         }
+        
+        [JsonProperty("use_ssl")]
+        private bool _useSsl
+        {
+            get
+            {
+                return useSsl;
+            }
+
+            set
+            {
+                useSsl = value;
+            }
+        }
 
         public RequiredMod[] requiredMods = null!;
         public string maximumAppVersion = null!;
+        /// <summary>
+        /// Request SSL (DTLS) connections when connecting to dedicated servers.
+        /// MultiplayerCore does NOT enforce this setting.
+        /// </summary>
+        public bool useSsl = false;
 
         [Serializable]
         public class RequiredMod
