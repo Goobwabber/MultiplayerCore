d50268a5a45497ba8af3a1d2d43b2ad4e52d9690
EnderdracheLP <enderdrachelp@gmail.com>
2023-12-28 18:56:01 +0100

    Use correct VarLong getter

--- MultiplayerCore/NodePoseSyncState/MpNodePoseSyncStatePacket.cs
+++ MultiplayerCore/NodePoseSyncState/MpNodePoseSyncStatePacket.cs
@@ -9,14 +9,14 @@ namespace MultiplayerCore.NodePoseSyncState
         public long fullStateUpdateFrequency = 100L;
         public override void Serialize(NetDataWriter writer)
         {
-            writer.Put(deltaUpdateFrequency);
-            writer.Put(fullStateUpdateFrequency);
+            writer.PutVarLong(deltaUpdateFrequency);
+            writer.PutVarLong(fullStateUpdateFrequency);
         }
 
         public override void Deserialize(NetDataReader reader)
         {
-            deltaUpdateFrequency = reader.GetLong();
-            fullStateUpdateFrequency = reader.GetLong();
+            deltaUpdateFrequency = reader.GetVarLong();
+            fullStateUpdateFrequency = reader.GetVarLong();
         }
     }
 }
