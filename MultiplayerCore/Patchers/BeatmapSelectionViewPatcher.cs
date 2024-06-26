c5146a64dce29c4182565c2679b8ff7dfd49e92a
EnderdracheLP <enderdrachelp@elp.quest>
2024-06-26 23:27:20 +0200

    Add compat for 1.35, code cleanup

--- MultiplayerCore/Patchers/BeatmapSelectionViewPatcher.cs
+++ MultiplayerCore/Patchers/BeatmapSelectionViewPatcher.cs
@@ -1,15 +1,14 @@
-﻿using MultiplayerCore.Beatmaps;
+﻿using HarmonyLib;
 using MultiplayerCore.Beatmaps.Abstractions;
-using MultiplayerCore.Beatmaps.Packets;
 using MultiplayerCore.Beatmaps.Providers;
 using MultiplayerCore.Objects;
+using SiraUtil.Affinity;
 using System;
 using System.Collections.Generic;
-using SiraUtil.Affinity;
 
 namespace MultiplayerCore.Patchers
 {
-    internal class BeatmapSelectionViewPatcher : IAffinity
+	internal class BeatmapSelectionViewPatcher : IAffinity
     {
         private MpPlayersDataModel _mpPlayersDataModel;
         private MpBeatmapLevelProvider _mpBeatmapLevelProvider;
@@ -32,7 +31,7 @@ namespace MultiplayerCore.Patchers
             if (_beatmapLevelsModel.GetBeatmapLevel(beatmapKey.levelId) != null) return true;
 
             var levelHash = Utilities.HashForLevelID(beatmapKey.levelId);
-            if (String.IsNullOrWhiteSpace(levelHash)) return true;
+            if (string.IsNullOrWhiteSpace(levelHash)) return true;
 
             var packet = _mpPlayersDataModel.FindLevelPacket(levelHash!);
             if (packet == null) return true;
@@ -42,7 +41,19 @@ namespace MultiplayerCore.Patchers
             __instance._levelBar.hide = false;
 
             var level = _mpBeatmapLevelProvider.GetBeatmapFromPacket(packet).MakeBeatmapLevel(beatmapKey, _mpBeatmapLevelProvider.MakeBeatSaverPreviewMediaData(packet.levelHash));
-			__instance._levelBar.SetupData(level, beatmapKey.difficulty, beatmapKey.beatmapCharacteristic);
+
+			// For 1.35
+			var mInfo = __instance._levelBar.GetType().GetMethod("Setup", new Type[] { level.GetType(), beatmapKey.beatmapCharacteristic.GetType(), beatmapKey.difficulty.GetType() });
+			if (mInfo != null)
+	            mInfo.Invoke(__instance._levelBar, new object[] { level, beatmapKey.beatmapCharacteristic, beatmapKey.difficulty });
+            else
+            {
+				// For 1.37
+	            mInfo = __instance._levelBar.GetType().GetMethod("SetupData", new Type[] { level.GetType(), beatmapKey.difficulty.GetType(), beatmapKey.beatmapCharacteristic.GetType() });
+	            if (mInfo != null)
+		            mInfo.Invoke(__instance._levelBar, new object[] { level, beatmapKey.difficulty, beatmapKey.beatmapCharacteristic });
+	            else Plugin.Logger.Critical("Can't find a fitting LevelBar Method, is your game version supported?");
+            }
 			return false;
         }
 
@@ -55,7 +66,7 @@ namespace MultiplayerCore.Patchers
             if (_beatmapLevelsModel.GetBeatmapLevel(beatmapKey.levelId) != null) return true;
 
             var levelHash = Utilities.HashForLevelID(beatmapKey.levelId);
-            if (String.IsNullOrWhiteSpace(levelHash)) return true;
+            if (string.IsNullOrWhiteSpace(levelHash)) return true;
 
             var packet = _mpPlayersDataModel.FindLevelPacket(levelHash!);
             if (packet == null) return true;
@@ -64,8 +75,19 @@ namespace MultiplayerCore.Patchers
             __instance._levelBar.hide = false;
 
             var level = _mpBeatmapLevelProvider.GetBeatmapFromPacket(packet).MakeBeatmapLevel(beatmapKey, _mpBeatmapLevelProvider.MakeBeatSaverPreviewMediaData(packet.levelHash));
-            __instance._levelBar.SetupData(level, beatmapKey.difficulty, beatmapKey.beatmapCharacteristic);
-            return false;
+
+
+			var mInfo = __instance._levelBar.GetType().GetMethod("Setup", new Type[] { level.GetType(), beatmapKey.beatmapCharacteristic.GetType(), beatmapKey.difficulty.GetType() });
+			if (mInfo != null)
+				mInfo.Invoke(__instance._levelBar, new object[] { level, beatmapKey.beatmapCharacteristic, beatmapKey.difficulty });
+			else
+			{
+	            mInfo = __instance._levelBar.GetType().GetMethod("SetupData", new Type[] { level.GetType(), beatmapKey.difficulty.GetType(), beatmapKey.beatmapCharacteristic.GetType() });
+				if (mInfo != null)
+				   mInfo.Invoke(__instance._levelBar, new object[] { level, beatmapKey.difficulty, beatmapKey.beatmapCharacteristic });
+                else Plugin.Logger.Critical("Can't find a fitting LevelBar Method, is your game version supported?");
+			}
+			return false;
         }
     }
 
@@ -73,38 +95,106 @@ namespace MultiplayerCore.Patchers
     {
         public static BeatmapLevel MakeBeatmapLevel(this MpBeatmap mpBeatmap, in BeatmapKey key, IPreviewMediaData previewMediaData) 
         {
-            var dict = new Dictionary<(BeatmapCharacteristicSO, BeatmapDifficulty), BeatmapBasicData>();
-            dict[(key.beatmapCharacteristic, key.difficulty)] = new BeatmapBasicData(
-                0, 
-                0, 
-                EnvironmentName.Empty, 
-                null, 
-                0, 
-                0, 
-                0, 
-                new[] { mpBeatmap.LevelAuthorName }, 
-                Array.Empty<string>()
-            );
+            var dict = new Dictionary<(BeatmapCharacteristicSO, BeatmapDifficulty), BeatmapBasicData>
+			{
+				[(key.beatmapCharacteristic, key.difficulty)] = new BeatmapBasicData(
+				0,
+				0,
+				EnvironmentName.Empty,
+				null,
+				0,
+				0,
+				0,
+				new[] { mpBeatmap.LevelAuthorName },
+				Array.Empty<string>()
+			)
+			};
 
-            return new BeatmapLevel(
-                0,
-                false,
-                mpBeatmap.LevelID,
-                mpBeatmap.SongName,
-                mpBeatmap.SongAuthorName,
-                mpBeatmap.SongSubName,
-                new[] { mpBeatmap.LevelAuthorName },
-                Array.Empty<string>(),
-                mpBeatmap.BeatsPerMinute,
-                -6.0f,
-                0,
-                0,
-                0,
-                mpBeatmap.SongDuration,
-                PlayerSensitivityFlag.Safe,
-                previewMediaData,
-                dict
-            );
+			// For 1.35
+			var conInfo = AccessTools.Constructor(typeof(BeatmapLevel), new Type[]
+			{
+				typeof(bool),
+				typeof(string),
+				typeof(string),
+				typeof(string),
+				typeof(string),
+				typeof(string[]),
+				typeof(string[]),
+				typeof(float),
+				typeof(float),
+				typeof(float),
+				typeof(float),
+				typeof(float),
+				typeof(float),
+				typeof(PlayerSensitivityFlag),
+				typeof(IPreviewMediaData),
+				typeof(IReadOnlyDictionary<(BeatmapCharacteristicSO, BeatmapDifficulty), BeatmapBasicData>)
+			});
+			if (conInfo != null)
+			{
+				return (BeatmapLevel)conInfo.Invoke(new object[]
+				{
+					false,
+					mpBeatmap.LevelID,
+					mpBeatmap.SongName,
+					mpBeatmap.SongAuthorName,
+					mpBeatmap.SongSubName,
+					new[] { mpBeatmap.LevelAuthorName },
+					Array.Empty<string>(),
+					mpBeatmap.BeatsPerMinute,
+					-6.0f,
+					0,
+					0,
+					0,
+					mpBeatmap.SongDuration,
+					PlayerSensitivityFlag.Safe,
+					previewMediaData,
+					dict
+				});
+			}
+			// For 1.37
+			conInfo = AccessTools.Constructor(typeof(BeatmapLevel), new Type[]
+			{
+				typeof(int),
+				typeof(bool),
+				typeof(string),
+				typeof(string),
+				typeof(string),
+				typeof(string),
+				typeof(string[]),
+				typeof(string[]),
+				typeof(float),
+				typeof(float),
+				typeof(float),
+				typeof(float),
+				typeof(float),
+				typeof(float),
+				typeof(PlayerSensitivityFlag),
+				typeof(IPreviewMediaData),
+				typeof(IReadOnlyDictionary<(BeatmapCharacteristicSO, BeatmapDifficulty), BeatmapBasicData>)
+			});
+			if (conInfo != null)
+				return (BeatmapLevel)conInfo.Invoke(new object[]
+				{
+					0,
+					false,
+					mpBeatmap.LevelID,
+					mpBeatmap.SongName,
+					mpBeatmap.SongAuthorName,
+					mpBeatmap.SongSubName,
+					new[] { mpBeatmap.LevelAuthorName },
+					Array.Empty<string>(),
+					mpBeatmap.BeatsPerMinute,
+					-6.0f,
+					0,
+					0,
+					0,
+					mpBeatmap.SongDuration,
+					PlayerSensitivityFlag.Safe,
+					previewMediaData,
+					dict
+				});
+			throw new NotSupportedException("Game Version not supported");
         }
     }
 }
