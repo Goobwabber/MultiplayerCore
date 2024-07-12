71c3b3ed85b133278fb85bf7b29d32c704e77cd9
EnderdracheLP <enderdrachelp@elp.quest>
2024-07-12 22:05:43 +0200

    No score validation for custom levels

--- MultiplayerCore/Patches/MultiplayerLevelFinishedControllerPatch.cs
+++ MultiplayerCore/Patches/MultiplayerLevelFinishedControllerPatch.cs
@@ -15,21 +15,14 @@ namespace MultiplayerCore.Patches
 		static bool HandleRpcLevelFinished(MultiplayerLevelFinishedController __instance, string userId, MultiplayerLevelCompletionResults results)
 		{
 			// Possibly get notesCount from BeatSaver or by parsing the beatmapdata ourselves
-			float maxMultipliedScore = ScoreModel.ComputeQuickInaccurateMaxMultipliedScoreForBeatmap(__instance._beatmapBasicData) * 1.21f;
-			if (!results.hasAnyResults)
-				Plugin.Logger.Info($"Score Received from user with id '{userId}' contains no results");
 			// Skip score validation if notesCount is 0, since custom songs always have notesCount 0 in BeatmapBasicData
 			// TODO: Change this to only be a single if (__instance._beatmapBasicData.notesCount <= 0 && results.hasAnyResults) if check
-			else if (__instance._beatmapBasicData.notesCount <= 0)
+			if (__instance._beatmapBasicData.notesCount <= 0 && results.hasAnyResults)
 			{
 				Plugin.Logger.Info($"BeatmapData noteCount is 0, skipping validation");
 				__instance._otherPlayersCompletionResults[userId] = results;
 				return false;
 			}
-			else if (results.levelCompletionResults.modifiedScore <= maxMultipliedScore && results.levelCompletionResults.modifiedScore >= 0)
-				Plugin.Logger.Info($"Score Received from user with id '{userId}' contains results and is valid, modifiedScore: '{results.levelCompletionResults.modifiedScore}'");
-			else
-				Plugin.Logger.Info($"Score Received from user with id '{userId}' failed validation, maxMultipliedScore: '{maxMultipliedScore}', modifiedScore: '{results.levelCompletionResults.modifiedScore}'");
 			return true;
 		}
 	}
