c99eb9c2d62db942401c443d0cc9534507250da8
EnderdracheLP <enderdrachelp@elp.quest>
2025-03-01 21:39:42 +0100

    Reformat code, log nested exception up to third, add todo

--- MultiplayerCore/Patches/GraphAPIClientPatch.cs
+++ MultiplayerCore/Patches/GraphAPIClientPatch.cs
@@ -4,32 +4,38 @@ using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using BGNet.Core.GameLift;
 using HarmonyLib;
-using SiraUtil.Affinity;
 
 namespace MultiplayerCore.Patches
 {
 	[HarmonyPatch]
 	internal class GraphAPIClientPatch
 	{
-		private static readonly HttpClient _client = new HttpClient();
+		private static readonly HttpClient _client = new();
 
-		private static readonly MethodInfo _sendAsyncAttacher = SymbolExtensions.GetMethodInfo(() => SendAsync(null!, 0, CancellationToken.None));
-		private static readonly MethodInfo _sendAsyncMethod = typeof(HttpClient).GetMethod(nameof(HttpClient.SendAsync), new Type[] { typeof(HttpRequestMessage), typeof(HttpCompletionOption), typeof(CancellationToken) })!;
+		// TODO: If this ever breaks, just patch the GraphAPIClient ctor and replace HttpClient _client with a custom HttpClient that has a logging handler
 
-		static MethodBase TargetMethod() =>
-			AccessTools.FirstInner(typeof(GraphAPIClient), t => t.Name.StartsWith("<Post>d__5`1"))?.MakeGenericType(typeof(GetMultiplayerInstanceResponse)).GetMethod("MoveNext", BindingFlags.NonPublic | BindingFlags.Instance)!;
+		private static readonly MethodInfo _sendAsyncAttacher =
+			SymbolExtensions.GetMethodInfo(() => SendAsync(null!, 0, CancellationToken.None));
+
+		private static readonly MethodInfo _sendAsyncMethod = typeof(HttpClient).GetMethod(nameof(HttpClient.SendAsync),
+			new[] { typeof(HttpRequestMessage), typeof(HttpCompletionOption), typeof(CancellationToken) })!;
+
+		private static MethodBase TargetMethod()
+		{
+			return AccessTools.FirstInner(typeof(GraphAPIClient), t => t.Name.StartsWith("<Post>d__5`1"))
+				?.MakeGenericType(typeof(GetMultiplayerInstanceResponse))
+				.GetMethod("MoveNext", BindingFlags.NonPublic | BindingFlags.Instance)!;
+		}
 
 		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
 		{
 			Plugin.Logger.Trace("Transpiling GraphAPIClient.Post");
 			var codes = instructions.ToList();
-			for (int i = 0; i < codes.Count; i++)
+			for (var i = 0; i < codes.Count; i++)
 			{
 				Plugin.Logger.Trace($"Instruction at index {i}: {codes[i].opcode}");
 				if (codes[i].opcode == OpCodes.Callvirt)
@@ -38,7 +44,7 @@ namespace MultiplayerCore.Patches
 					if (codes[i].Calls(_sendAsyncMethod))
 					{
 						Plugin.Logger.Trace("Found SendAsync call");
-						CodeInstruction newCode = new CodeInstruction(OpCodes.Callvirt, _sendAsyncAttacher);
+						var newCode = new CodeInstruction(OpCodes.Callvirt, _sendAsyncAttacher);
 						codes[i] = newCode;
 					}
 				}
@@ -47,10 +53,10 @@ namespace MultiplayerCore.Patches
 			return codes.AsEnumerable();
 		}
 
-		private static Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption,
+		private static Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+			HttpCompletionOption completionOption,
 			CancellationToken cancellationToken)
 		{
-
 			Plugin.Logger.Debug("SendAsync MasterServer Request called");
 			var result = _client.SendAsync(request, completionOption, cancellationToken);
 
@@ -58,19 +64,19 @@ namespace MultiplayerCore.Patches
 			{
 				try
 				{
-					HttpResponseMessage response = await task;
+					var response = task.Result;
 					if (!response.IsSuccessStatusCode)
 					{
 						Plugin.Logger.Error(
 							$"An error occurred while attempting to post to the Graph API: Uri '{request.RequestUri}' StatusCode: {(int)response.StatusCode}: {response.StatusCode}");
-						Plugin.Logger.Trace($"Response: {response.Content.ReadAsStringAsync()}");
+						Plugin.Logger.Trace($"Response: {await response.Content.ReadAsStringAsync()}");
 					}
-
 				}
 				catch (Exception ex)
 				{
 					Plugin.Logger.Error(
-						$"An error occurred while attempting to post to the Graph API: Uri '{request.RequestUri}' Exception Message: {ex.Message + (ex.InnerException != null ? " --> " + ex.InnerException.Message : "")}");
+						$"An error occurred while attempting to post to the Graph API: Uri '{request.RequestUri}' Exception Message: " +
+						$"{ex.Message + (ex.InnerException != null ? " --> " + ex.InnerException.Message : "") + (ex.InnerException != null && ex.InnerException.InnerException != null ? " --> " + ex.InnerException.InnerException.Message : "")}");
 					Plugin.Logger.Trace(ex);
 				}
 			});
